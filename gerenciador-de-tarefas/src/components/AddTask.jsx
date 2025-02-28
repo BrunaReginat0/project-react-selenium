c98153b51f44fe4d925a407c6132705a056d569e
Bruna Santana Reginato <b.santana.reginato@avanade.com>
2025-02-28 15:17:24 -0300

    estrutura fo projeto

---
+++ gerenciador-de-tarefas/src/components/AddTask.jsx
@@ -0,0 +1,5 @@
+function AddTask() {
+  <h2>Add Tasks</h2>;
+}
+
+export default AddTask;
---
+++ gerenciador-de-tarefas/src/components/Tasks.jsx
@@ -0,0 +1,5 @@
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  return <ul className="space-y-4 "></ul>;
+}
