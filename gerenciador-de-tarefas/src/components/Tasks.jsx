a2a2e74769b15ae7017433f1a9019afe06b26824
Bruna Santana Reginato <b.santana.reginato@avanade.com>
2025-03-03 16:36:19 -0300

    lucide dando erro

--- gerenciador-de-tarefas/src/components/Tasks.jsx
+++ gerenciador-de-tarefas/src/components/Tasks.jsx
@@ -1,5 +1,29 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
-  return <ul className="space-y-4 "></ul>;
+  return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => onTaskClick(task.id)}
+            className={`bg-slate-400 text-left w-full text-black p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-black">
+            <ChevronRightIcon />
+          </button>
+          <button
+            onClick={() => onDeleteTaskClick(task.id)}
+            className="bg-slate-400 p-2 rounded-md text-black"
+          >
+            <TrashIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
